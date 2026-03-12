bac784ec1ce2f164852daf40fec4c3704239ac64
developerdivonyx <miravirani2403@gmail.com>
2026-03-12 14:42:48 +0530

    socket bug

--- src/services/socketConntection.js
+++ src/services/socketConntection.js
@@ -1,62 +1,6 @@
-// import { io } from "socket.io-client";
-// import { getDecryptedToken, getTenantId } from "../utils/functions/tokenEncryption";
-// import { getDispatcherId } from "../utils/auth";
-
-// let socket = null;
-
-// const initSocket = () => {
-//     if (socket) return socket;
-
-//     const tenantId = getTenantId();
-//     const token = getDecryptedToken();
-//     const dispatcher_id = getDispatcherId()
-
-//     console.log("Socket database:", tenantId);
-//     console.log("Socket dispatcher_id:", dispatcher_id);
-//     console.log("token===", token);
-
-
-//     if (!tenantId) {
-//         console.warn("❌Tenant ID not found, socket not connected");
-//         return null;
-//     }
-
-//     socket = io("https://backend.cabifyit.com", {
-//         path: "/socket.io",
-//         transports: ["polling", "websocket",],
-//         reconnection: true,
-//         reconnectionAttempts: 5,
-//         reconnectionDelay: 2000,
-//         query: {
-//             role: "dispatcher",
-//             dispatcher_id: dispatcher_id,
-//             database: tenantId,
-//         },
-//         extraHeaders: {
-//             Authorization: `Bearer ${token}`
-//         }
-//     });
-
-
-//     socket.on("connect", () => {
-//         console.log("Socket connected:", socket.id);
-//     });
-
-//     socket.on("disconnect", (reason) => {
-//         console.log("Socket disconnected:", reason);
-//     });
-
-//     socket.on("connect_error", (error) => {
-//         console.error("⚠️ Socket connection error:", error.message);
-//     });
-
-//     return socket;
-// };
-
-// export default initSocket;
-
 import { io } from "socket.io-client";
 import { getDecryptedToken, getTenantId } from "../utils/functions/tokenEncryption";
+import { getDispatcherId } from "../utils/auth";
 
 let socket = null;
 
@@ -64,12 +8,13 @@ const initSocket = () => {
     if (socket) return socket;
 
     const tenantId = getTenantId();
-    // const companyId = getCompanyId()
     const token = getDecryptedToken();
+    const dispatcher_id = getDispatcherId()
 
-    console.log("tenantId:", tenantId);
-    console.log("companyId:", tenantId);
+    console.log("Socket database:", tenantId);
+    console.log("Socket dispatcher_id:", dispatcher_id);
     console.log("token===", token);
+
 
     if (!tenantId) {
         console.warn("❌Tenant ID not found, socket not connected");
@@ -78,19 +23,20 @@ const initSocket = () => {
 
     socket = io("https://backend.cabifyit.com", {
         path: "/socket.io",
-        transports: ["polling", "websocket"],
+        transports: ["polling", "websocket",],
         reconnection: true,
         reconnectionAttempts: 5,
         reconnectionDelay: 2000,
         query: {
-            role: "client",
-            client_id: tenantId,
+            role: "dispatcher",
+            dispatcher_id: dispatcher_id,
             database: tenantId,
         },
         extraHeaders: {
             Authorization: `Bearer ${token}`
         }
     });
+
 
     socket.on("connect", () => {
         console.log("Socket connected:", socket.id);
@@ -108,3 +54,57 @@ const initSocket = () => {
 };
 
 export default initSocket;
+
+// import { io } from "socket.io-client";
+// import { getDecryptedToken, getTenantId } from "../utils/functions/tokenEncryption";
+
+// let socket = null;
+
+// const initSocket = () => {
+//     if (socket) return socket;
+
+//     const tenantId = getTenantId();
+//     // const companyId = getCompanyId()
+//     const token = getDecryptedToken();
+
+//     console.log("tenantId:", tenantId);
+//     console.log("companyId:", tenantId);
+//     console.log("token===", token);
+
+//     if (!tenantId) {
+//         console.warn("❌Tenant ID not found, socket not connected");
+//         return null;
+//     }
+
+//     socket = io("https://backend.cabifyit.com", {
+//         path: "/socket.io",
+//         transports: ["polling", "websocket"],
+//         reconnection: true,
+//         reconnectionAttempts: 5,
+//         reconnectionDelay: 2000,
+//         query: {
+//             role: "client",
+//             client_id: tenantId,
+//             database: tenantId,
+//         },
+//         extraHeaders: {
+//             Authorization: `Bearer ${token}`
+//         }
+//     });
+
+//     socket.on("connect", () => {
+//         console.log("Socket connected:", socket.id);
+//     });
+
+//     socket.on("disconnect", (reason) => {
+//         console.log("Socket disconnected:", reason);
+//     });
+
+//     socket.on("connect_error", (error) => {
+//         console.error("⚠️ Socket connection error:", error.message);
+//     });
+
+//     return socket;
+// };
+
+// export default initSocket;
